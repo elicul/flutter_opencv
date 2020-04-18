ebd94296404b5b5a2d42c29a99a55e2f773159aa
Enzo Licul <enzol@ebe-inc.com>
2020-04-18 22:51:16 +0200

    Initial

---
+++ ios/Classes/FlutterOpencvPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterOpencvPlugin.h"
+
+@implementation FlutterOpencvPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_opencv"
+            binaryMessenger:[registrar messenger]];
+  FlutterOpencvPlugin* instance = [[FlutterOpencvPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
